e377cf757f85a69a365d2893dce2aad83a39e7a7
fatih <fatihkabbani223@gmail.com>
2023-04-11 19:29:34 +0100

    fixed the inline issuse with the buttons and the title and image in li

--- scripts/content.js
+++ scripts/content.js
@@ -1,11 +1,10 @@
-const bookmarksList = document.getElementById('bookmarks-list');
-let inputToCreateAFolder = document.getElementById('input_to_create_a_folder');
+const bookmarksList = document.getElementById("bookmarks-list");
+let inputToCreateAFolder = document.getElementById("input_to_create_a_folder");
 chrome.bookmarks.getTree(async (bookmarkTreeNodes) => {
   for (const node of bookmarkTreeNodes) {
     if (node.children) {
       await addBookmarks(node.children, bookmarksList);
-    }
-    else {
+    } else {
       bookmarksList.appendChild(createListItem(node.title, node.url, node.id));
     }
   }
@@ -16,36 +15,35 @@ async function addBookmarks(bookmarkNodes, parent) {
   for (const node of bookmarkNodes) {
     if (node.children) {
       // If the node is a folder, create a new details element for it
-      const details = document.createElement('details');
-      details.classList.add('bookmark-item');
+      const details = document.createElement("details");
+      details.classList.add("bookmark-item");
       // force open the details (folder)
-      details.setAttribute('open', 'true');
+      details.setAttribute("open", "true");
       // #############################################
-      const summary = document.createElement('summary');
-      const image = document.createElement('img');
-      const pTag = document.createElement('p');
-      summary.classList.add('bookmark-link');
+      const summary = document.createElement("summary");
+      const image = document.createElement("img");
+      const pTag = document.createElement("p");
+      summary.classList.add("bookmark-link");
       summary.appendChild(image);
       pTag.innerText = node.title;
       summary.appendChild(pTag);
-      pTag.classList.add('detailspTag');
+      pTag.classList.add("detailspTag");
       image.src = "icon/open-folder.png";
       image.alt = "image";
-      image.classList.add('no-image');
+      image.classList.add("no-image");
       details.appendChild(summary);
 
       // Create a new ul element to store the bookmarks inside the folder
-      const ul = document.createElement('ul');
+      const ul = document.createElement("ul");
       await addBookmarks(node.children, ul);
 
       // Append the ul element to the details element
       details.appendChild(ul);
-    // add image to details folder 
-    
+      // add image to details folder
+
       // Append the details element to the parent element
 
       parent.appendChild(details);
-      
     } else {
       // If the node is a bookmark, create a new li element for it
       parent.appendChild(createListItem(node.title, node.url, node.id));
@@ -56,27 +54,30 @@ async function addBookmarks(bookmarkNodes, parent) {
 function createListItem(title, url, id) {
   // if it's folder make it a details make the bookmark inside the details
 
-
-  const li = document.createElement('li');
-  li.classList.add('bookmark-item');
+  const li = document.createElement("li");
+  li.classList.add("bookmark-item");
   const a = `<a href="${url ? url : "#"}" class="bookmark-link">${title}</a>`;
   const buttonDelete = `<button id="delete" value="${id}">delete</button>`;
   const buttonEdit = `<button id="edit" value="${id}">edit</button>`;
-  const image = `<img src="${url ? "https://s2.googleusercontent.com/s2/favicons?domain=" + url : "icon/open-folder.png"} " alt="image" class="no-image">`;
-  li.innerHTML = `${image}${a}${buttonEdit}${buttonDelete}`;
-  li.querySelector('#delete').addEventListener('click', deleteBookMark);
-  li.querySelector('#edit').addEventListener('click', createElement);
+  const image = `<img src="${
+    url
+      ? "https://s2.googleusercontent.com/s2/favicons?domain=" + url
+      : "icon/open-folder.png"
+  } " alt="image" class="no-image">`;
+  li.innerHTML = `<span class="imgandlinkspan">${image}${a}</span><span class="buttons">${buttonEdit}${buttonDelete}</span>`;
+  li.querySelector("#delete").addEventListener("click", deleteBookMark);
+  li.querySelector("#edit").addEventListener("click", createElement);
   return li;
 }
-
-// get current tab 
+// TODO make the edit a popup 
+// get current tab
 function getCurrentTab() {
   return new Promise((resolve, reject) => {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
       if (tabs.length > 0) {
         resolve(tabs[0]);
       } else {
-        reject('No active tabs found in the last focused window.');
+        reject("No active tabs found in the last focused window.");
       }
     });
   });
@@ -87,7 +88,7 @@ async function getCurrentTabIndex() {
     const tabIndex = value.index;
     return tabIndex;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 async function getCurrentTabUrl() {
@@ -96,24 +97,25 @@ async function getCurrentTabUrl() {
     const tabUrl = value.url;
     return tabUrl;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 async function getCurrentTabTitle() {
   try {
     const value = await getCurrentTab();
-    const tabTitle = value.title
-    return tabTitle
-
+    const tabTitle = value.title;
+    return tabTitle;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 //Todo to override the extension page :
 
 //! create onclick function to create a folder when user click
-document.getElementById('create_new_bookmark').addEventListener('click', createNewBookMark)
+document
+  .getElementById("create_new_bookmark")
+  .addEventListener("click", createNewBookMark);
 async function createNewBookMark() {
   chrome.bookmarks.getTree(async function (bookmarkTreeNodes) {
     let bookmarkBar = bookmarkTreeNodes[0].children[0].id;
@@ -121,64 +123,75 @@ async function createNewBookMark() {
     try {
       let tabTitle = await getCurrentTabTitle();
       let tabUrl = await getCurrentTabUrl();
-      chrome.bookmarks.create({
-        'parentId': bookmarkBar,
-        'title': tabTitle,
-        'url': tabUrl
-      }, function (newBookmark) {
-        console.log("Added bookmark: " + newBookmark.title);
-      });
+      chrome.bookmarks.create(
+        {
+          parentId: bookmarkBar,
+          title: tabTitle,
+          url: tabUrl,
+        },
+        function (newBookmark) {
+          console.log("Added bookmark: " + newBookmark.title);
+        }
+      );
     } catch (error) {
       console.log(error);
     }
   });
 }
-document.getElementById('create_new_folder').addEventListener('click', createNewFolder)
+document
+  .getElementById("create_new_folder")
+  .addEventListener("click", createNewFolder);
 async function createNewFolder() {
   chrome.bookmarks.getTree(async function (bookmarkTreeNodes) {
     let bookmarkBar = bookmarkTreeNodes[0].children[0].id;
-    chrome.bookmarks.create({
-      'parentId': bookmarkBar,
-      'title': inputToCreateAFolder.value
-    }, function (newFolder) {
-      console.log("Added folder: " + newFolder.title);
-    });
-  })
+    chrome.bookmarks.create(
+      {
+        parentId: bookmarkBar,
+        title: inputToCreateAFolder.value,
+      },
+      function (newFolder) {
+        console.log("Added folder: " + newFolder.title);
+      }
+    );
+  });
 }
-// delete bookmark 
+// delete bookmark
 function deleteBookMark() {
   let bookmarkId = String(this.value);
-  chrome.bookmarks.remove(bookmarkId)
+  chrome.bookmarks.remove(bookmarkId);
 }
 
 //TODO update changes (user change's (url , title))
 
 function createElement(event) {
   const id = event.target.value;
-  const listItem = event.target.closest('li');
-  const link = listItem.querySelector('.bookmark-link');
+  const listItem = event.target.closest("li");
+  const link = listItem.querySelector(".bookmark-link");
   const title = link.innerText;
   const url = link.href;
   // Create input fields for editing the bookmark title and URL
-  const titleInput = document.createElement('input');
-  titleInput.type = 'text';
-  titleInput.placeholder = 'Title';
+  const titleInput = document.createElement("input");
+  titleInput.type = "text";
+  titleInput.placeholder = "Title";
   titleInput.value = title;
 
-  const urlInput = document.createElement('input');
-  urlInput.type = 'text';
-  urlInput.placeholder = 'URL';
+  const urlInput = document.createElement("input");
+  urlInput.type = "text";
+  urlInput.placeholder = "URL";
   urlInput.value = url;
 
   // Create a submit button for saving the changes
-  const saveButton = document.createElement('button');
-  saveButton.innerText = 'Save';
-  saveButton.addEventListener('click', async () => {
+  const saveButton = document.createElement("button");
+  saveButton.innerText = "Save";
+  saveButton.addEventListener("click", async () => {
     const newTitle = titleInput.value;
     const newUrl = urlInput.value;
 
     // Update the bookmark with the new title and URL
-    const updatedBookmark = await chrome.bookmarks.update(id, { title: newTitle, url: newUrl });
+    const updatedBookmark = await chrome.bookmarks.update(id, {
+      title: newTitle,
+      url: newUrl,
+    });
 
     // Update the link text and href in the list item
     link.innerText = updatedBookmark.title;
@@ -198,22 +211,27 @@ function createElement(event) {
 
 // get user input
 function getUserInput() {
-  let userInput = document.getElementById('input_to_create_a_folder').value;
-  return userInput
+  let userInput = document.getElementById("input_to_create_a_folder").value;
+  return userInput;
 }
 // edit bookmark
 // TODO make it work
 // suucessfull edit bookmark
 function susscefullEditBookMark(bookMarkOrFolder) {
-  console.log(bookMarkOrFolder.title)
+  console.log(bookMarkOrFolder.title);
 }
 // rejected edit bookmark
 function rejectedEditBookMark() {
-  console.log(`ERROR: ${this} unable to edit bookmark or folder`)
+  console.log(`ERROR: ${this} unable to edit bookmark or folder`);
 }
 // edit bookmark function
 function updateBookmarkOrFolder(id) {
   let userInput = getUserInput();
   let bookmarkId = String(this.value);
-  chrome.bookmarks.update(bookmarkId, { title: userInput }, susscefullEditBookMark, rejectedEditBookMark)
+  chrome.bookmarks.update(
+    bookmarkId,
+    { title: userInput },
+    susscefullEditBookMark,
+    rejectedEditBookMark
+  );
 }
